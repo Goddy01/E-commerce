d545040bfcbe9a51a24c322679b89109f610fa50
Godwin Adigun <adigungodwin2@gmail.com>
2022-04-18 11:39:19 +0100

    added contact.html

--- store/views.py
+++ store/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
 class DefView(TemplateView):
-    template_name = 'checkout.html'
+    template_name = 'contact.html'
