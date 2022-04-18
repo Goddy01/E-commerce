37a5794714fc4d089b048ec96fe9a2755a68db9c
Godwin Adigun <adigungodwin2@gmail.com>
2022-04-18 12:53:18 +0100

    added shop.html

--- store/views.py
+++ store/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
 class DefView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'shop.html'
