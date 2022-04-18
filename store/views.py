e0191e27cbbcfc17819376bbd8a99ed3d98fb164
Godwin Adigun <adigungodwin2@gmail.com>
2022-04-18 12:43:53 +0100

    added index.html

--- store/views.py
+++ store/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
 class DefView(TemplateView):
-    template_name = 'detail.html'
+    template_name = 'index.html'
