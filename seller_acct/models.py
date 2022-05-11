cbfcd8d48307b6e46aa2ffafa4df6fb794b40d13
Godwin Adigun <adigungodwin2@gmail.com>
2022-05-11 21:29:37 +0100

    changed phonenum_field to regex

--- seller_acct/models.py
+++ seller_acct/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
-from phonenumber_field.modelfields import PhoneNumberField
+from django.core.validators import RegexValidator
 # Create your models here.
 
 
@@ -9,5 +9,6 @@ class Vendor_Acct(AbstractBaseUser):
     fullname =                      models.CharField(blank=False, null=False, max_length=256)
     username =                      models.Charfield(blank=False, null=False, max_length=256, unique=True)
     address =                       models.CharField(blank=False, null=False, max_length=512)
-    first_phone_number =            PhoneNumberField(null=False, blank=False, verbose_name='first phone number')
-    second_phone_number =           PhoneNumberField(null=False, blank=False)
+    phone_regex =                   RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+123456789'. Up to 15 digits allowed.")
+    first_phone_number =            models.CharField(validators=[phone_regex], max_length=17, blank=False, null=False) # Validators should be a list
+    second_phone_number =           models.CharField(validators=[phone_regex], max_length=17, blank=False, null=False) # Validators should be a list
