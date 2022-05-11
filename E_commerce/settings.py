90503da77c03dd48670bed46c4c15176daeac66d
Godwin Adigun <adigungodwin2@gmail.com>
2022-05-11 21:22:47 +0100

    added phonenumber_field lib

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'phonenumber_field',
 
     # My Apps
     'seller_acct',
@@ -134,3 +135,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# PHONWNUMBERFIELD
+PHONENUMBER_DB_FORMAT = 'NATIONAL'
+PHONENUMBER_DEFAULT_REGION = 'US'
--- seller_acct/models.py
+++ seller_acct/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 
+
+
 class Vendor_Acct(AbstractBaseUser):
-    fullname =          models.CharField(blank=False, null=False, max_length=256)
-    username =          models.Charfield(blank=False, null=False, max_length=256, unique=True)
-    address =           models.CharField(blank=False, null=False, max_length=512)
-    
+    fullname =                      models.CharField(blank=False, null=False, max_length=256)
+    username =                      models.Charfield(blank=False, null=False, max_length=256, unique=True)
+    address =                       models.CharField(blank=False, null=False, max_length=512)
+    first_phone_number =            PhoneNumberField(null=False, blank=False, verbose_name='first phone number')
+    second_phone_number =           PhoneNumberField(null=False, blank=False)
