76146391482580c645af702f3e736fd3fcbb7017
Godwin Adigun <adigungodwin2@gmail.com>
2022-05-11 20:24:55 +0100

    added some vendors required fields

--- seller_acct/models.py
+++ seller_acct/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 # Create your models here.
+
+class Vendor_Acct(AbstractBaseUser):
+    fullname =          models.CharField(blank=False, null=False, max_length=256)
+    username =          models.Charfield(blank=False, null=False, max_length=256, unique=True)
+    address =           models.CharField(blank=False, null=False, max_length=512)
+    
