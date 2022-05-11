e9c296aed5b190acf8b1fd02529b4ec7cf6a9ead
Godwin Adigun <adigungodwin2@gmail.com>
2022-05-11 22:12:13 +0100

    added verification to views

--- seller_acct/views.py
+++ seller_acct/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import VendorRegForm
 
 # Create your views here.
 def vendor_reg_view(request):
-    
+    context = {}
+    if request.method == 'POST':
+        form = VendorRegForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            context['success_message'] = 'Account Creation Successful!'
+            return redirect(login)
+        else:
+            form = VendorRegForm()
+    else:
+        context['form'] = VendorRegForm()
+    return render(request, 'login.html', context)
