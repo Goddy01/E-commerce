716c50a2e21fb583630511ad41b702c226b5ee3f
Godwin Adigun <adigungodwin2@gmail.com>
2022-05-11 21:36:26 +0100

    made few changes in models

--- seller_acct/models.py
+++ seller_acct/models.py
@@ -10,5 +10,5 @@ class Vendor_Acct(AbstractBaseUser):
     username =                      models.Charfield(blank=False, null=False, max_length=256, unique=True)
     address =                       models.CharField(blank=False, null=False, max_length=512)
     phone_regex =                   RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+123456789'. Up to 15 digits allowed.")
-    first_phone_number =            models.CharField(validators=[phone_regex], max_length=17, blank=False, null=False) # Validators should be a list
-    second_phone_number =           models.CharField(validators=[phone_regex], max_length=17, blank=False, null=False) # Validators should be a list
+    first_phone_num =            models.CharField(validators=[phone_regex], max_length=17, blank=False, null=False, verbose_name='first phone num') # Validators should be a list
+    second_phone_num =           models.CharField(validators=[phone_regex], max_length=17, blank=False, null=False, verbose_name='second phone num') # Validators should be a list
