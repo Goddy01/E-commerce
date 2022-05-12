948f7dbcb86cc6b0a39d114012688812a1b79e59
Godwin Adigun <adigungodwin2@gmail.com>
2022-05-12 21:14:14 +0100

    added email in forms

--- seller_acct/forms.py
+++ seller_acct/forms.py
@@ -4,6 +4,7 @@ from .models import Vendor_Acct
 
 class VendorRegForm(UserCreationForm):
     """The form for vendors to register their accounts."""
+    email = forms.EmailField(max_length=128, help_text="Provide a valid email address.")
     class Meta:
         model = Vendor_Acct
         fields = ('fullname', 'username', 'address', 'first_phone_num', 'second_phone_num', 'email')
