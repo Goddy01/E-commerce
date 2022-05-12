9170de04b112d3176aab4403e064bc41042a3315
Godwin Adigun <adigungodwin2@gmail.com>
2022-05-12 21:50:14 +0100

    added vendor's login view

--- seller_acct/views.py
+++ seller_acct/views.py
@@ -1,7 +1,7 @@
 from multiprocessing import context
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
-from .forms import VendorRegForm
+from .forms import VendorRegForm, VendorLoginForm
 
 # Create your views here.
 def vendor_reg_view(request):
@@ -19,3 +19,25 @@ def vendor_reg_view(request):
     else:
         context['form'] = VendorRegForm()
     return render(request, 'seller_acct/register.html', context)
+
+
+
+def vendor_login_view(request):
+    context = {}
+    if request.method == 'POST':
+        form = VendorLoginForm(request.POST)
+
+        if form.is_valid():
+            email = request.POST.get('email').lower()
+            password = request.POST.get('password')
+
+            user = authenticate(email=email, password=password)
+            if user:
+                login(request, user)
+                return redirect('dashboard')
+    else:
+        form = VendorLoginForm()
+    context['form'] = form
+    return render(request, 'seller_acct/login.html', context)
+
+
