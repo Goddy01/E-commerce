49ad4866b9c3b9ec27a82551fd76e1ccf6cd82c8
Godwin Adigun <adigungodwin2@gmail.com>
2022-05-12 21:22:46 +0100

    added login form

--- seller_acct/forms.py
+++ seller_acct/forms.py
@@ -1,5 +1,7 @@
+from xml.dom import ValidationErr
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, SetPasswordForm
+from django.contrib.auth import authenticate
 from .models import Vendor_Acct
 
 class VendorRegForm(UserCreationForm):
@@ -8,3 +10,19 @@ class VendorRegForm(UserCreationForm):
     class Meta:
         model = Vendor_Acct
         fields = ('fullname', 'username', 'address', 'first_phone_num', 'second_phone_num', 'email')
+
+
+class VendorLoginForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput, label='Password')
+    class Meta:
+        model = Vendor_Acct
+        fields = ('email', 'password')
+
+    def clean(self):
+        if self.is_valid():
+            email = self.cleaned_data.get('email').lower()
+            password = self.cleaned_data.get('password')
+            user = authenticate(email=email, password=password)
+
+            if not user:
+                raise forms.ValidationError('Invalid login details')
