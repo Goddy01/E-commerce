96f770a64af13d4d5badb51b4e6f19be9ab92d4a
Godwin Adigun <adigungodwin2@gmail.com>
2022-05-12 20:54:02 +0100

    changed verbose names

--- seller_acct/models.py
+++ seller_acct/models.py
@@ -38,8 +38,8 @@ class Vendor_Acct(AbstractBaseUser):
     email =                         models.EmailField(blank=False, null=False)
     address =                       models.CharField(blank=False, null=False, max_length=512)
     phone_regex =                   RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+123456789'. Up to 15 digits allowed.")
-    first_phone_num =            models.CharField(validators=[phone_regex], max_length=17, blank=False, null=False, verbose_name='first phone num') # Validators should be a list
-    second_phone_num =           models.CharField(validators=[phone_regex], max_length=17, blank=False, null=False, verbose_name='second phone num') # Validators should be a list
+    first_phone_num =            models.CharField(validators=[phone_regex], max_length=17, blank=False, null=False, verbose_name='Phone No 1') # Validators should be a list
+    second_phone_num =           models.CharField(validators=[phone_regex], max_length=17, blank=False, null=False, verbose_name='Phone No 2') # Validators should be a list
     date_joined             = models.DateTimeField(auto_now_add=True)
     last_login              = models.DateTimeField(auto_now=True)
     is_admin                = models.BooleanField(default=False)
