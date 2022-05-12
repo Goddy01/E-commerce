52a945190547d090e6c74dc78f9ff35cd0baecd9
Godwin Adigun <adigungodwin2@gmail.com>
2022-05-12 20:50:51 +0100

    updated with email

--- seller_acct/forms.py
+++ seller_acct/forms.py
@@ -6,4 +6,4 @@ class VendorRegForm(UserCreationForm):
     """The form for vendors to register their accounts."""
     class Meta:
         model = Vendor_Acct
-        fields = ('fullname', 'username', 'address', 'first_phone_num', 'second_phone_num')
+        fields = ('fullname', 'username', 'address', 'first_phone_num', 'second_phone_num', 'email')
--- seller_acct/models.py
+++ seller_acct/models.py
@@ -35,6 +35,7 @@ class AccountManager(BaseUserManager):
 class Vendor_Acct(AbstractBaseUser):
     fullname =                      models.CharField(blank=False, null=False, max_length=256)
     username =                      models.CharField(blank=False, null=False, max_length=256, unique=True)
+    email =                         models.EmailField(blank=False, null=False)
     address =                       models.CharField(blank=False, null=False, max_length=512)
     phone_regex =                   RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+123456789'. Up to 15 digits allowed.")
     first_phone_num =            models.CharField(validators=[phone_regex], max_length=17, blank=False, null=False, verbose_name='first phone num') # Validators should be a list
@@ -48,7 +49,7 @@ class Vendor_Acct(AbstractBaseUser):
 
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['fullname', 'username', 'address', 'first_phone_num', 'second_phone_num']
+    REQUIRED_FIELDS = ['fullname', 'username', 'address', 'first_phone_num', 'second_phone_num', 'email']
 
     objects = AccountManager()
 
