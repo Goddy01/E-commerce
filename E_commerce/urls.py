4c283ed48d8065a88260bcc66528879cc1677a2d
Godwin Adigun <adigungodwin2@gmail.com>
2022-05-25 16:03:55 +0100

    added password_reset_complete url

--- E_commerce/urls.py
+++ E_commerce/urls.py
@@ -45,7 +45,8 @@ urlpatterns = [
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name="password/password_change_done.html"), name="password change done"),
 
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='password/password_change.html'), name='password change'),
-    # path(),
+    
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password/password_reset_complete.html'), name="password reset complete"),
 ]
 
 
