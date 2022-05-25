86db5278784203aaf4ac0ab045a2173bd19c3e6f
Godwin Adigun <adigungodwin2@gmail.com>
2022-05-25 15:32:09 +0100

    added home url to base

--- E_commerce/urls.py
+++ E_commerce/urls.py
@@ -41,7 +41,8 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="password/password_reset_done.html"), name="pasword reset done"),
     
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password/password_reset_form.html'), name="password reset confirm"),
-    # path(),
+    
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name="password/password_change_done.html"), name="password change done"),
     # path(),
     # path(),
 ]
