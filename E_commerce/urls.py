7fe77edfe2e389f0c65ae2b527493d7998ed3a88
Godwin Adigun <adigungodwin2@gmail.com>
2022-05-25 15:23:28 +0100

    made few adjustments in password_reset_done

--- E_commerce/urls.py
+++ E_commerce/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
 
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password/password_reset.html'), name='password reset'),
     
-    path('password_reset/done', auth_views.PasswordResetCompleteView.as_view(template_name="password/password_reset_done.html"), name="pasword_reset_done"),
+    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="password/password_reset_done.html"), name="pasword reset done"),
     
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password/password_reset_form.html'), name="password reset confirm"),
     # path(),
