cff369a29d2b0b31c06242cab2adcd5ef8cdd3ad
Godwin Adigun <adigungodwin2@gmail.com>
2022-06-01 17:23:17 +0100

    improved the forms

--- Vendors_Acct/forms.py
+++ Vendors_Acct/forms.py
@@ -1,20 +1,20 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, SetPasswordForm
 from django.contrib.auth import authenticate
-from .models import Vendor_Acct
+from .models import User_Acct
 
-class VendorRegForm(UserCreationForm):
+class RegForm(UserCreationForm):
     """The form for vendors to register their accounts."""
     email = forms.EmailField(max_length=128, help_text="Provide a valid email address.")
     class Meta:
-        model = Vendor_Acct
+        model = User_Acct
         fields = ['fullname', 'username', 'address', 'first_phone_num', 'second_phone_num', 'email','password1', 'password2']
 
 
-class VendorLoginForm(forms.ModelForm):
+class LoginForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput, label='Password')
     class Meta:
-        model = Vendor_Acct
+        model = User_Acct
         fields = ['email', 'password']
 
     def clean(self):
