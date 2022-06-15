63f5151469e37a7a1c1c72debbc11c2267193042
Godwin Adigun <adigungodwin2@gmail.com>
2022-06-15 19:04:30 +0100

    added form auth for vendors

--- Accounts/forms.py
+++ Accounts/forms.py
@@ -25,3 +25,25 @@ class CustomerLoginForm(forms.ModelForm):
 
             if not user:
                 raise forms.ValidationError('Invalid login details')
+class VendorRegForm(UserCreationForm):
+    """The form for Customers to register their accounts."""
+    email = forms.EmailField(max_length=128, help_text="Provide a valid email address.")
+    class Meta:
+        model = Vendor
+        fields = ['fullname', 'username', 'address', 'first_phone_num', 'second_phone_num', 'email','password1', 'password2']
+
+
+class CustomerLoginForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput, label='Password')
+    class Meta:
+        model = Vendor
+        fields = ['email', 'password']
+
+    def clean(self):
+        if self.is_valid():
+            email = self.cleaned_data['email'].lower()
+            password = self.cleaned_data['password']
+            user = authenticate(email=email, password=password)
+
+            if not user:
+                raise forms.ValidationError('Invalid login details')
