a8c0d0070c4cb2eac591561a31fd22dc9666aa0e
Godwin Adigun <adigungodwin2@gmail.com>
2022-06-17 17:05:09 +0100

    added the logout url

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,4 +1,4 @@
-from .views import vendor_reg_view, user_login_view, customer_reg_view
+from .views import vendor_reg_view, user_login_view, customer_reg_view, user_logout_view
 from django.urls import path
 
 app_name = 'user'
@@ -6,5 +6,6 @@ app_name = 'user'
 urlpatterns = [
     path('v_reg/', vendor_reg_view, name='v_reg'),
     path('c_reg/', customer_reg_view, name='c_reg'),
-    path('login/', user_login_view, name='login')
+    path('login/', user_login_view, name='login'),
+    path('logout/', user_logout_view, name='logout'),
 ]
