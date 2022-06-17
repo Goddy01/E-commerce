04fbfe52335fd76b8ba20c5b54bab56477539c9e
Godwin Adigun <adigungodwin2@gmail.com>
2022-06-17 17:25:45 +0100

    added the urls for each user typr

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,4 +1,4 @@
-from .views import vendor_reg_view, user_login_view, customer_reg_view, user_logout_view
+from .views import vendor_reg_view, vendor_login_view, customer_reg_view, customer_login_view, user_logout_view
 from django.urls import path
 
 app_name = 'user'
@@ -6,6 +6,7 @@ app_name = 'user'
 urlpatterns = [
     path('v_reg/', vendor_reg_view, name='v_reg'),
     path('c_reg/', customer_reg_view, name='c_reg'),
-    path('login/', user_login_view, name='login'),
+    path('v_login/', vendor_login_view, name='v_login'),
+    path('c_login/', customer_login_view, name="c_login"),
     path('logout/', user_logout_view, name='logout'),
 ]
