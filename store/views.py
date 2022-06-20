a0f772a33a95cda3d240d15be69864c41b981283
Godwin Adigun <adigungodwin2@gmail.com>
2022-06-20 13:22:38 +0100

    added add_product_view

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from .forms import AddProduct
+from Accounts.models import Vendor
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'index.html'
@@ -22,6 +24,22 @@ class ContactView(TemplateView):
 # class AddProductView(TemplateView):
 #     template_name = 'create_product.html'
 
-def add_product(request):
-    model = Product
+def add_product_view(request):
+    """The view for vendors to add products to the store"""
+    user = request.user
     
+    if not user.is_authenticated:
+        return redirect('user:login')
+
+    context = {}
+    add_form = AddProduct(request.POST or None, request.FILES or None)
+    if add_form.is_valid():
+        add_form.save()
+        # vendor = Vendor.objects.filter(email=user.email)
+        # print(vendor)
+        context['success_message'] = "Product has been posted."
+        return redirect('home')
+    else:
+        add_form = AddProduct()
+    context['add_product_form'] = add_form
+    return render(request,'store/create_product.html',context)
