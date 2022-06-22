a1f406e99d0a91a7517cf1bb23a814d0d8f940a8
Godwin Adigun <adigungodwin2@gmail.com>
2022-06-22 17:29:52 +0100

    added category choices to form

--- store/forms.py
+++ store/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 from .models import Product
-
+from .models import category_choices
 
 class AddProductForm(forms.ModelForm):
+    category = forms.MultipleChoiceField(choices=category_choices, widget=forms.CheckboxSelectMultiple())
+
     class Meta():
         model = Product
-        fields = ('product_description', 'product_image3', 'product_image2', 'product_image1', 'product_colors', 'number_available', 'product_sizes', 'product_price', 'product_name')
+        fields = ('product_description', 'product_image3', 'product_image2', 'product_image1', 'product_colors', 'number_available', 'product_sizes', 'product_price', 'product_name', 'category_choices')
     # instance = ""
