6818a44fd584a196f7bfaad0da7fe0de63f7f872
Godwin Adigun <adigungodwin2@gmail.com>
2022-07-27 16:13:37 +0100

    fixed the querydoesnotexist bug for unauth users inw_c

--- store/context_processor.py
+++ store/context_processor.py
@@ -7,19 +7,15 @@ def website_content(request):
     if request.user.is_authenticated:
         order, created = Order.objects.get_or_create(customer=request.user, complete=False)
         items = order.orderitem_set.all()
-        try:
-            subtotal = order.get_cart_total
-            cart_items = order.get_cart_items
-            for item in items:
-                if item.product.number_available == 0:
-                    cart_items -= item.quantity
-                if item.quantity > item.product.number_available:
-                    cart_items -= item.quantity
-                    cart_items += item.product.number_available
-            print('CartItems is ', cart_items)
-        except AttributeError:
-            subtotal = 0
-            cart_items = 0
+        subtotal = order.get_cart_total
+        cart_items = order.get_cart_items
+        for item in items:
+            if item.product.number_available == 0:
+                cart_items -= item.quantity
+            if item.quantity > item.product.number_available:
+                cart_items -= item.quantity
+                cart_items += item.product.number_available
+        print('CartItems is ', cart_items)
     else:
         try:
             cart = json.loads(request.COOKIES['cart'])
@@ -29,24 +25,27 @@ def website_content(request):
         items = []
         cart_items = order['get_cart_items']
         for item in cart:
-            cart_items += cart[item]['quantity']
+            try:
+                cart_items += cart[item]['quantity']
 
-            product = Product.objects.get(product_id=item)
-            order['get_cart_total'] += (product.product_price * cart[item]['quantity'])
-            order['get_cart_items'] += cart[item]['quantity']
+                product = Product.objects.get(product_id=item)
+                order['get_cart_total'] += (product.product_price * cart[item]['quantity'])
+                order['get_cart_items'] += cart[item]['quantity']
 
-            item = {
-                'product':{
-                    'product_id': product.product_id,
-                    'product_name': product.product_name,
-                    'product_image1': product.product_image1,
-                    'product_price': product.product_price,
-                    'number_available': product.number_available,
-                },
-                'quantity': cart[item]['quantity'],
-                'get_items_price': cart[item]['quantity'] * product.product_price,
-            }
-            items.append(item)
+                item = {
+                    'product':{
+                        'product_id': product.product_id,
+                        'product_name': product.product_name,
+                        'product_image1': product.product_image1,
+                        'product_price': product.product_price,
+                        'number_available': product.number_available,
+                    },
+                    'quantity': cart[item]['quantity'],
+                    'get_items_price': cart[item]['quantity'] * product.product_price,
+                }
+                items.append(item)
+            except:
+                pass
         for item in items:
             if item['product']['number_available'] == 0:
                 items.remove(item)
