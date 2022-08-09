75e6ce8d2ceead6b91b544e96056ad1f01a1cc03
Godwin Adigun <adigungodwin2@gmail.com>
2022-08-09 11:31:57 +0100

    added website context

--- store/context_processor.py
+++ store/context_processor.py
@@ -1,18 +1,23 @@
-from Accounts.models import User
-from django.shortcutsimport request
+from Accounts.models import Customer
+from .models import Order
+from django.shortcuts import request
 def website_conent(request):
-    try:
-        user = User.objects.get(device=request.session['device'])
-    except:
-        device = str(uuid.uuid4())
-        request.session['device'] = device
-        user = User.objects.create(
-            fullname=request.session['device'],
-            username=request.session['device'],
-            email=request.session['device'] + '@gmail.com',
-            address=request.session['device'],
-            first_phone_num='1',
-            device=request.session['device'],
-
-            )
-    return user
+    if not request.user.is_authenticated:
+        customer = Customer.objects.get(device=request.session.get('device'))
+    else:
+        customer = Customer.objects.get(email=request.user.email)
+    order = Order.objects.filter(customer=customer).order_by('-id').first()
+    items = order.orderitem_set.all()
+    subtotal = order.get_cart_total
+    cart_items = order.get_cart_items
+    for item in items:
+        if item.product.number_available == 0:
+            cart_items -= item.quantity
+        if item.quantity > item.product.number_available:
+            cart_items -= item.quantity
+            cart_items += item.product.number_available
+    context = {
+        'items':items, 
+        'cart_items': cart_items,
+                    }
+    return context
