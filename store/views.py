495ad59f0e9871faeab5b192647ac46d0b2029e6
Godwin Adigun <adigungodwin2@gmail.com>
2022-08-23 18:52:16 +0100

    done integrating the pagination functionality

--- store/views.py
+++ store/views.py
@@ -27,29 +27,10 @@ class CustomPhoneProvider(Provider):
                     parsed_number,
                     phonenumbers.PhoneNumberFormat.E164
                 )
-# def store(request):
-#     if request.user.is_authenticated:
-#         order, created = Order.objects.get_or_create(customer=request.user, complete=False)
-#         items = order.orderitem_set.all()
-#         cart_items = order.get_cart_items
-#     else:
-#         try:
-#             cart = json.loads(request.session['cart'])
-#         except KeyError:
-#             cart = {}
-#         order = {'get_cart_total':0, 'get_cart_items':0}
-#         items = []
-#         cart_items = order['get_cart_items']
-#         for item in cart:
-#             cart_items += cart[item]['quantity']
-#     context = {'items':items, 'cart_items':cart_items}
-    # return render(request, 'store/index.html', context)
 
 class ShopView(TemplateView):
     template_name = 'shop.html'
 
-# class ShopDetailView(TemplateView):
-#     template_name = 'detail.html'
 
 class ContactView(TemplateView):
     template_name = 'contact.html'
@@ -423,15 +404,3 @@ def get_product_queryset(request):
             return render(request, 'store/index.html')
     else:
         return render(request, 'store/index.html')
-
-# def pagination(request):
-#     products_list = Product.objects.all()
-#     page_number =   request.GET.get('page', 1)
-#     products_paginator = Paginator(products_list, 6)
-#     try:
-#         products = products_paginator.page(page_number)
-#     except PageNotAnInteger:
-#         products = products_paginator.page(1)
-#     except EmptyPage:
-#         products = products_paginator.page(products_paginator.num_pages)
-#     return render(request, 'store/index.html', {'p_products': products})
