5e183701b33160d4274dc4a00528bb6abcf93971
Godwin Adigun <adigungodwin2@gmail.com>
2022-08-27 15:53:55 +0100

    added ProductAdmin

--- store/admin.py
+++ store/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Product, Order, OrderItem, BillingAddress, Review
 # Register your models here.
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ('date_added',)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(BillingAddress)
