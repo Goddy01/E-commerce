a759069d298fcdf38a52232c2f7aa5cf559f2e94
Godwin Adigun <adigungodwin2@gmail.com>
2022-08-30 11:44:35 +0100

    added Wallet model in walletApp.models

--- walletApp/models.py
+++ walletApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from Accounts.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Wallet(models.Model):
+    user = models.OneToOneField(
+        User, null=True, on_delete=models.CASCADE)
+    currency = models.CharField(max_length=50, default='NGN')
+    created_at = models.DateTimeField(default=timezone.now, null=True)
+
+     def __str__(self):
+        return self.user.fullname
