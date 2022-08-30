bdb86b947f555ba54fdcebeb8c825b006e17221e
Godwin Adigun <adigungodwin2@gmail.com>
2022-08-30 12:38:25 +0100

    added the urls for walletApp

--- walletApp/urls.py
+++ walletApp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
+from .views import WalletInfo, DepositFunds, VerifyDeposit
 app_name = 'wallet'
 
 urlpatterns = [
-
+    path('wallet_info/', WalletInfo.as_view(), name="wallet_info"),
+    path('deposit/', DepositFunds.as_view(), name="deposit"),
+    path('deposit/verify/<str:reference>/', VerifyDeposit.as_view(), name="verify_deposit"),
 ]
