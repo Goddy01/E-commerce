a70a5a1041e691ff36099844ebfee48b5e012b4f
Godwin Adigun <adigungodwin2@gmail.com>
2022-08-30 12:31:20 +0100

    added WalletInfo view in walletApp.views

--- walletApp/views.py
+++ walletApp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.conf import settings
+from .models import Wallet
+from .serializers import WalletSerializer, DepositSerializer
 
-# Create your views here.
+class WalletInfo(APIView):
+
+    def get(self, request):
+        wallet = Wallet.objects.get(user=request.user)
+        data = WalletSerializer(wallet).data
+        return Response(data)
