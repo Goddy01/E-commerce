efe168c5aa6117e6fb946f80b159660644a1f176
Godwin Adigun <adigungodwin2@gmail.com>
2022-08-30 12:28:37 +0100

    added DepositSerializer

--- walletApp/serializers.py
+++ walletApp/serializers.py
@@ -25,3 +25,32 @@ def is_amount(value):
     if value <= 0:
         raise serializers.ValidationError({"detail": "Invalid Amount"})
     return value
+
+class DepositSerializer(serializers.Serializer):
+
+    amount = serializers.IntegerField(validators=[is_amount])
+    email = serializers.EmailField()
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            return value
+        raise serializers.ValidationError({"detail": "Email not found"})
+
+    def save(self):
+        user = self.context['request'].user
+        wallet = Wallet.objects.get(user=user)
+        data = self.validated_data
+        url = 'https://api.paystack.co/transaction/initialize'
+        headers = {
+            {"authorization": f"Bearer {settings.PAYSTACK_SECRET_KEY}"}}
+        r = requests.post(url, headers=headers, data=data)
+        response = r.json()
+        WalletTransaction.objects.create(
+            wallet=wallet,
+            transaction_type="deposit",
+            amount= data["amount"],
+            paystack_payment_reference=response['data']['reference'],
+            status="pending",
+        )
+
+        return response
