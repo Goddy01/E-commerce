8ee157746a8115c1b0fef4fd7f95838148897e15
Godwin Adigun <adigungodwin2@gmail.com>
2022-08-30 16:31:47 +0100

    refactored initiate_payment view

--- walletApp/views.py
+++ walletApp/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from .forms import PaymentForm
 from datetime import datetime
 from django.contrib import messages
+from django.conf import settings
+from django.http import HttpRequest, HttpResponse
 
-def initiate_payment(request):
+def initiate_payment(request: HttpRequest, amount, email) -> HttpResponse:
     if request.method == 'POST':
         payment_form = PaymentForm(request.POST)
         if payment_form.is_valid():
-            payment = payment_form.save()
-            render(request, 'make_payment.html', {'payment': payment})
+            payment = payment_form.save(commit=False)
+            payment.amount = amount
+            payment.email = email
+            payment.save()
+            render(request, 'make_payment.html', {'payment': payment, 'paystack_public_key': settings.PAYSTACK_PUBLIC_KEY})
         else:
             payment_form = PaymentForm()
     return render(request, 'initiate_payment.html', {'payment_form': payment_form})
