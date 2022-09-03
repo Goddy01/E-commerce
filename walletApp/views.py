711fb87efba656024004b480b17c10622a791a6f
Godwin Adigun <adigungodwin2@gmail.com>
2022-09-03 14:14:31 +0100

    making sure only rightful owners can make-payment

--- walletApp/views.py
+++ walletApp/views.py
@@ -3,15 +3,25 @@ from django.views.decorators.http import require_http_methods
 from datetime import datetime
 from django.contrib import messages
 from django.conf import settings
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpResponse
 from store.models import BillingAddress, Order
+from Accounts.models import Vendor
 
 def make_payment(request):
-    order = Order.objects.filter(customer__email=request.user.email).order_by('-id').first()
-    request.session['total'] = order.total_order_price
-    request.session['email'] = request.user.email
-    if request.session.get('total') == order.total_order_price:
-        order.complete = not order.complete
-    order.save()
-    # email = order.customer.email
+    vendors = Vendor.objects.all()
+    for vendor in vendors:
+        if vendor.username == request.user.username:
+            return HttpResponse("You must create a Customer Account to be able to access customers' privileges.")
+    if request.user.is_authenticated:
+        order = Order.objects.filter(customer__email=request.user.email).order_by('-id').first()
+        if order.total_order_price is None:
+            return redirect('home')
+        else:
+            request.session['total'] = order.total_order_price
+            request.session['email'] = request.user.email
+            if request.session.get('total') == order.total_order_price:
+                order.complete = not order.complete
+            order.save()
+    else:
+        return redirect('user:must_auth')
     return render(request, 'make_payment.html', {'total': request.session.get('total'), 'email': request.session.get('email')})
