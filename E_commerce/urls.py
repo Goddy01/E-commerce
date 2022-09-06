cf8b84db49fd129b07a57b1c7bb89ebbb5ebd62e
Godwin Adigun <adigungodwin2@gmail.com>
2022-09-06 14:14:23 +0100

    added male_children_category url where necessary

--- E_commerce/urls.py
+++ E_commerce/urls.py
@@ -35,6 +35,7 @@ from store.views import (
     add_to_wishlist, remove_from_wishlist,
     wishlist, remove_from_wishlist_view,
     men_category,women_category,
+    male_children_category,
     )
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -58,6 +59,7 @@ urlpatterns = [
     path('cart/', cart, name='cart'),
     path('men_category/', men_category, name='men_category'),
     path('women_category/', women_category, name='women_category'),
+    path('male_children_category/', male_children_category, name='male_children_category'),
     path('cart/<item_id>/increment/', quantity_increment, name='q_incr'),
     path('cart/<item_id>/decrement/', quantity_decrement, name='q_decr'),
     path('cart/<item_id>/delete/', delete_order_item, name='del_item'),
