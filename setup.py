c608a670f7ac3469fef7bf558fb9cb40dfba75f1
Godwin Adigun <adigungodwin2@gmail.com>
2022-09-10 18:05:16 +0100

    fixing setup bug

--- setup.py
+++ setup.py
@@ -1,16 +1,47 @@
-from setuptools import setup
-from pypaystack import version
+import os
+from setuptools import find_packages, setup
 
-setup(name='pypaystack',
-    version=version.__version__,
-    description='Python wrapper for Paystack API',
-    url='https://github.com/edwardpopoola/pypaystack',
-    author=version.__author__,
-    author_email='edwardpopoola@gmail.com',
-    license=version.__license__,
-    test_suite='nose.collector',
-    tests_require=['nose'],
-    install_requires=['requests'],
-    packages=['pypaystack'],
-    zip_safe=False
-    )
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name="pypaystack",
+    version="1.24",
+    packages=find_packages(),
+    include_package_data=True,
+    license="MIT License",  # example license
+    description="A reusable app for making online payments with paystack",
+    long_description=README,
+    url="https://www.example.com/",
+    author="Biola Oyeniyi",
+    author_email="gbozee@gmail.com",
+    install_requires=[
+        "requests",
+        "future",
+        "Paperboy",
+        "python-dateutil",
+        # "requests-async",
+        # "requests-async @ https://github.com/encode/requests-async/archive/master.zip",
+    ],
+    extras_require={"django": ["django>=2.0"], "starlette": ["starlette==0.12.13"]},
+    # dependency_links=[
+    # "http://github.com/SergeySatskiy/cdm-pythonparser/archive/v2.0.1.tar.gz"
+    # ],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: X.Y",  # replace "X.Y" as appropriate
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",  # example license
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        # Replace these appropriately if you are stuck on Python 2.
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+)
