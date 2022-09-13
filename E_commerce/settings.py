3c17c3ace4985e2920555d0aedcad8da106b87d4
Godwin Adigun <adigungodwin2@gmail.com>
2022-09-13 12:42:56 +0100

    added debug exception

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -34,6 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = ['summit.up.railway.app']
 
@@ -217,37 +218,38 @@ SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 
 if not DEBUG:
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-    LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
+    
+LOGGING = {
+'version': 1,
+'disable_existing_loggers': False,
+'formatters': {
+    'verbose': {
+        'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+        'datefmt' : "%d/%b/%Y %H:%M:%S"
     },
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'mysite.log',
-            'formatter': 'verbose'
-        },
+    'simple': {
+        'format': '%(levelname)s %(message)s'
     },
-    'loggers': {
-        'django': {
-            'handlers':['file'],
-            'propagate': True,
-            'level':'DEBUG',
-        },
-        'MYAPP': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-        },
-    }
+},
+'handlers': {
+    'file': {
+        'level': 'DEBUG',
+        'class': 'logging.FileHandler',
+        'filename': 'mysite.log',
+        'formatter': 'verbose'
+    },
+},
+'loggers': {
+    'django': {
+        'handlers':['file'],
+        'propagate': True,
+        'level':'DEBUG',
+    },
+    'MYAPP': {
+        'handlers': ['file'],
+        'level': 'DEBUG',
+    },
+}
 }
 
 STATIC_HOST = "d1q43jnb3s7abw.cloudfront.net" if not DEBUG else ""
