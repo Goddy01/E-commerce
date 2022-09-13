f8922a8539c24a6b2c79f69110eac326f292898e
Godwin Adigun <adigungodwin2@gmail.com>
2022-09-13 11:29:02 +0100

    fixing bad request(400)

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://summit.up.railway.app/']
+ALLOWED_HOSTS = ['summit.up.railway.app/']
 
 
 # Application definition
